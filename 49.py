779f875801406b3d7954f01dcd49d41394aa7ad0
davis stanko <davisbvstanko@gmail.com>
2023-07-25 16:38:49 -0400

    49

---
+++ 49.py
@@ -0,0 +1,11 @@
+# Given an array of strings strs, group the anagrams together.
+
+def groupAnagrams(strs):
+    anagrams = {}
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
+        else:
+            anagrams[sorted_word] = [word]
+    return list(anagrams.values())
